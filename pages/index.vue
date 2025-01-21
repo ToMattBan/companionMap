8e7d91e020130fb4c1a589f70a15e0d097d825ca
ToMattBan <souaquelecleyton@yandex.com>
2025-01-21 17:25:10 -0300

    Moving to nuxt

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script lang="ts" setup>
+import mapUtils from '../utils/map';
+
+const attribuitions: string[] = [
+  'Maps from <a href="https://www.worldofgothic.com/">World of Gothic</a>',
+  'Markers from <a href="https://www.xboxachievements.com/forum/topic/239955-arcania-gothic-4-collectables-guide/">Xbox Achievements</a>',
+  'Made with <a href="https://leafletjs.com">Leaflet</a>'
+];
+
+const iconSize: L.PointTuple = [25, 25];
+
+onMounted(async () => {
+  mapUtils.initMap();
+  mapUtils.setTileMap('gothic_4', attribuitions.join(' | '));
+
+  const icons = {
+    adanos: mapUtils.createIcon('gothic_4', 'adanos.webp', iconSize, 'adanos-icon'),
+    innos: mapUtils.createIcon('gothic_4', 'innos.webp', iconSize, 'innos-icon'),
+    beliar: mapUtils.createIcon('gothic_4', 'beliar.webp', iconSize, 'beliar-icon')
+  }
+})
+</script>
+
+<style scoped>
+#map {
+  background-color: black;
+}
+</style>
---
+++ utils/map.ts
@@ -0,0 +1,46 @@
+import * as L from 'leaflet';
+
+type TGame = 'gothic_4';
+
+const baseUrl = process.env.NODE_ENV === 'production' 
+  ? '_nuxt/assets/'
+  : '_nuxt/assets/';
+
+let map: L.Map;
+
+function initMap() {
+  map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 2,
+    preferCanvas: true,
+    zoomControl: false
+  })
+
+  new L.Control.Zoom({ position: 'topright' }).addTo(map)
+}
+
+function setTileMap(game: TGame, attribuitions: string) {
+  const tilesUrl = `${baseUrl}/${game}/map/tiles/{z}/{x}/{y}.jpg`
+
+  L.tileLayer(tilesUrl, {
+    minZoom: 1,
+    maxZoom: 4,
+    noWrap: true,
+    attribution: attribuitions
+  }).addTo(map);
+}
+
+function createIcon(game: TGame, name: string, size: L.PointTuple, className?: string) {
+
+  L.icon({
+    iconUrl: `${baseUrl}/${game}/icons/${name}`,
+    iconSize: size,
+    className,
+  })
+}
+
+export default {
+  initMap,
+  setTileMap,
+  createIcon
+};
